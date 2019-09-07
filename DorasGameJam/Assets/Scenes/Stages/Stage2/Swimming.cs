7721dab01783c825ac1e7973dd0203f454a93613
cherio <cherio7777777@gmail.com>
2019-09-07 16:47:55 +0900

    ステージ２完成

--- DorasGameJam/Assets/Scenes/Stages/Stage2/Swimming.cs
+++ DorasGameJam/Assets/Scenes/Stages/Stage2/Swimming.cs
@@ -4,28 +4,23 @@ using UnityEngine;
 
 public class Swimming : MonoBehaviour
 {
-    GameObject _swimming1;
-    GameObject _swimming2;
+    Vector3 vec;
+    public float first;
     // Start is called before the first frame update
     void Start()
     {
-        _swimming1 = GameObject.Find("Swimming1");
-        _swimming2 = GameObject.Find("Swimming2");
-
+        vec.x = first;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var vec = Vector3.zero;
-        var vec2 = Vector3.zero;
-        vec.x = 1;
-        vec2.x = 1;
-        _swimming1.transform.position += vec * Time.deltaTime; 
-        _swimming2.transform.position -= vec2 * Time.deltaTime;
+        transform.position -= vec * Time.deltaTime;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        float y = 180;
+        this.transform.Rotate(0.0f, y, 0.0f);
+        vec.x *= -1;
     }
 }
