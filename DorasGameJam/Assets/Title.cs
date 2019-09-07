692899c7518cc680439370638570bc4aae9a5319
Pninari <saitokosei3411@gmail.com>
2019-09-07 14:37:14 +0900

    タイトルとタイトルの動作を追加しました。ゲームを終わるのボタンの処理はまだありません。

---
+++ DorasGameJam/Assets/Title.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+
+    public void PushEndButton()
+    {
+        SceneManager.LoadScene("Title");
+    }
+
+    public void PushStartButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+}
